cdc355e52ed62b3688c1c812aaaff2e6ab2927c6
Andrew Robles <70864612+andrewrobles@users.noreply.github.com>
2022-05-21 05:55:57 -0700

    Time: 88 ms (18.99%), Space: 13.9 MB (77.19%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,5 +1,6 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
+        """Time: O(n), Space: O(1)"""
         index, integer, values = 0, 0, {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         while index < len(s):
             symbol = s[index]
