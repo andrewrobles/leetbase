2c9febd934ce923bb813e0ad334e08041d314aea
Andrew Robles <70864612+andrewrobles@users.noreply.github.com>
2022-05-21 05:23:42 -0700

    Time: 74 ms (67.90%), Space: 13.8 MB (96.69%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        digits = [digit for digit in str(x)]
+        i, j = 0, len(digits) - 1
+        while i < j:
+            if digits[i] != digits[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
