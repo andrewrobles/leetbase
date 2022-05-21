56d3cde9dcb5e21e4ea313b2e2b00176b6a48fe9
Andrew Robles <70864612+andrewrobles@users.noreply.github.com>
2022-05-21 05:04:48 -0700

    Time: 7715 ms (5.00%), Space: 14.9 MB (95.18%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,7 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """Time: O(n^2) | Space: O(1)"""        
         for i, number_i in enumerate(nums):
             for j, number_j in enumerate(nums):
                 if i != j and number_i + number_j == target:
                     return [i, j]
-        
