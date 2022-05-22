482cb255f989e680a58dc0eb9da7a3ee244883ec
Andrew Robles <70864612+andrewrobles@users.noreply.github.com>
2022-05-22 06:52:06 -0700

    Time: 66 ms (15.34%), Space: 13.9 MB (50.07%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        longest_prefix = ''
+        longest_word = max(strs, key=len)       
+        for i in range(0, len(longest_word)+1):
+            current_prefix = longest_word[:i]
+            for j, word in enumerate(strs):
+                if word[:i] != current_prefix:
+                    return longest_prefix
+                elif j == len(strs) - 1:
+                    longest_prefix = max(current_prefix, longest_prefix, key=len)
+        return longest_prefix
