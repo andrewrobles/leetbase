fcadbb203b0db9da461e1c8677d824deaa87311c
Andrew Robles <70864612+andrewrobles@users.noreply.github.com>
2022-05-23 08:29:29 -0700

    Time: 132 ms (5.30%), Space: 14.4 MB (11.83%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.py
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -1,13 +1,35 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        """Time: O(n^2), Space: O(n)"""
-        longest_prefix = ''
-        longest_word = max(strs, key=len)       
-        for i in range(0, len(longest_word)+1):
-            current_prefix = longest_word[:i]
-            for j, word in enumerate(strs):
-                if word[:i] != current_prefix:
-                    return longest_prefix
-                elif j == len(strs) - 1:
-                    longest_prefix = max(current_prefix, longest_prefix, key=len)
-        return longest_prefix
+        prefixes = []
+        if len(strs) == 1:
+            return strs[0]
+        longest_common_prefix = ''
+        for i in range(len(strs) - 1):
+            current_word = strs[i]
+            next_word = strs[i+1]
+            current_prefix = ""
+            for j in range(len(current_word)):
+                if j < len(next_word) and current_word[j] == next_word[j]:
+                    current_prefix = current_word[:j + 1]
+                    print(f'current_prefix: {current_prefix}')
+                else:
+                    # longest_common_prefix = current_prefix
+                    # prefixes.append(current_prefix)
+                    # print(f'prefixes: {prefixes}')
+                    break
+            prefixes.append(current_prefix)
+            print(f'prefixes: {prefixes}')
+        return min(prefixes) if len(prefixes) != 0 else ""
+        # return longest_common_prefix
+        
+        # """Time: O(n^2), Space: O(n)"""
+        # longest_prefix = ''
+        # longest_word = max(strs, key=len)       
+        # for i in range(0, len(longest_word)+1):
+        #     current_prefix = longest_word[:i]
+        #     for j, word in enumerate(strs):
+        #         if word[:i] != current_prefix:
+        #             return longest_prefix
+        #         elif j == len(strs) - 1:
+        #             longest_prefix = max(current_prefix, longest_prefix, key=len)
+        # return longest_prefix
