32ac6660b2ca1281de7983e63c925904aa618b5e
Andrew Robles <70864612+andrewrobles@users.noreply.github.com>
2022-06-02 10:40:47 -0700

    Time: 35 ms (43.14%), Space: 13.4 MB (83.28%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -5,6 +5,7 @@
 #         self.next = next
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
+        """Time: O(n), Space: O(1)"""
         if not list1 and not list2:
             return list1
         elif list1 and not list2:
@@ -18,16 +19,16 @@ class Solution(object):
         else:
             head = list2
             list2 = list2.next
+        
         curr = head
         while list1 and list2:
             if list1.val < list2.val:
                 curr.next = list1
                 list1 = list1.next
-                curr = curr.next
             else:
                 curr.next = list2
                 list2 = list2.next
-                curr = curr.next
+            curr = curr.next
         if list1:
             curr.next = list1
         if list2:
