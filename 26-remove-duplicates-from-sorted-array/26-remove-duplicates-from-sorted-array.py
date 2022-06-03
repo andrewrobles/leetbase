2b998c78f3fe038508fd7ad58120ffc9efc94117
Andrew Robles <70864612+andrewrobles@users.noreply.github.com>
2022-06-03 07:55:09 -0700

    Time: 129 ms (30.16%), Space: 14.4 MB (89.44%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        1 1 2
+        i
+            j
+        """ 
+        i = 0
+        j = 1
+            
+        while j < len(nums):
+            if nums[i] != nums[j]:
+                nums[i + 1], nums[j] = nums[j], nums[i + 1]
+                i += 1
+            j += 1
+        return i + 1
+        
+        
+            
+        
