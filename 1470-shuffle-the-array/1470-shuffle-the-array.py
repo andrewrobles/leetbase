98eecc9ed45897bccbe43fb0464bfd738ff87c5f
Andrew Robles <70864612+andrewrobles@users.noreply.github.com>
2022-06-04 08:20:32 -0700

    Time: 54 ms (54.78%), Space: 13.5 MB (91.85%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,18 +1,6 @@
 class Solution(object):
     def shuffle(self, nums, n):
-        """
-        Example:
-        11 21 12 22 13 23
-        0     2     4
-           1     3     5
-        First n elements map to i * 2
-        Second n elements map to i * 2 + 1
-        
-        Approach:
-        - Create a new array of size 2 * n
-        - Place first n elements in i * 2 indices
-        - Place second n elements in i * 2 + 1 indices
-        """
+        """Time: O(n), Space: O(n)"""
         array = [None] * (2*n)
         for i in range(0, n):
             array[2*i] = nums[i]
