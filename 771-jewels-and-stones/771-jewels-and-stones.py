94fcecc17d7996dfdc55c9688c718b04933b24f0
Andrew Robles <70864612+andrewrobles@users.noreply.github.com>
2022-06-04 08:52:55 -0700

    Time: 23 ms (58.25%), Space: 13.3 MB (94.95%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.py
+++ 771-jewels-and-stones/771-jewels-and-stones.py
@@ -1,10 +1,13 @@
 class Solution(object):
     def numJewelsInStones(self, jewels, stones):
         """Time: O(n), Space: O(n)"""
+        count = 0
         freqs = {}
         for s in stones:
-            freqs[s] = 1 if s not in freqs else freqs[s] + 1
-        count = 0
+            if s not in freqs:
+                freqs[s] = 1
+            else:
+                freqs[s] += 1 
         for j in jewels:
             if j in freqs:
                 count += freqs[j]
