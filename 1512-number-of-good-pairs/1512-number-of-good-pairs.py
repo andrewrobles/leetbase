f01f45d924426c7a38d9cba7ff62460f13ceab5b
Andrew Robles <70864612+andrewrobles@users.noreply.github.com>
2022-06-05 14:01:07 -0700

    Time: 17 ms (91.29%), Space: 13.4 MB (36.15%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,11 +1,18 @@
 class Solution(object):
     def numIdenticalPairs(self, nums):
-        """Time: O(n^2), Space: O(1)"""
+        """
+        1 2 3 1 1 3
+        
+        """
+        freqs = {}
+        for number in nums:
+            if number in freqs:
+                freqs[number] += 1
+            else:
+                freqs[number] = 1
         count = 0
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if nums[i] == nums[j] and i < j:
-                    count += 1
+        for number, freq in freqs.items():
+            count += (freq * (freq - 1)// 2)
         return count
                     
         
