ab1e3fcdaa4cad3bb98f89c25a925f5209a59b80
Andrew Robles <70864612+andrewrobles@users.noreply.github.com>
2022-06-05 17:12:29 -0700

    Time: 3660 ms (17.50%), Space: 13.7 MB (29.06%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
@@ -1,26 +1,26 @@
 class Solution(object):
     def subArrayRanges(self, nums):
-        res = 0
-        for i, x in enumerate(nums):
-            minv = x
-            maxv = x
-            for  y in nums[i+1:]:
-                minv = min(minv, y)
-                maxv = max(maxv, y)
-                res += maxv-minv 
-        return res
-    
-#     def subArrayRanges(self, nums):
-#         """Time: O(n^2), Space: O(1)"""
-#         range_sum = 0
-#         for length in range(1, len(nums) + 1):
-#             start = 0
-#             stop = start + length
-#             while stop <= len(nums):
-#                 subarray = nums[start:stop]
-#                 range_sum += (max(subarray) - min(subarray))
-#                 start += 1
-#                 stop = start + length
-#         return range_sum
+        """
+        Order of subarray sum computation:
+        1
+        1 2
+        1 2 3
+        2
+        2 3
+        3
+        """
+        subarray_sum = 0
+        for i, start in enumerate(nums):
+            minimum = start
+            maximum = start
+            for curr in nums[i+1:]:
+                minimum = min(minimum, curr)
+                maximum = max(maximum, curr)
+                subarray_sum += (maximum - minimum)
+        return subarray_sum
+                
+                
+        
+        
                 
         
