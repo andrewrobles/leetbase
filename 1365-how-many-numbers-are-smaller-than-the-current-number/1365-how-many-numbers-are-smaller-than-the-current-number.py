5a4105c196e779019ee5a8b77669e1a2bb5820af
Andrew Robles <70864612+andrewrobles@users.noreply.github.com>
2022-06-11 07:45:46 -0700

    Time: 177 ms (53.04%), Space: 14 MB (46.59%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,12 +1,24 @@
 class Solution:
-    # O(n^2) time | O(1) space
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        array = []
-        for i in range(len(nums)):
-            count = 0
-            for j in range(len(nums)):
-                if i != j and nums[i] > nums[j]:
-                    count += 1
-            array.append(count)
-        return array
+        # Final list to be returned
+        final_list = []      
+        
+        # Make copy of nums list, but sorted
+        nums_sorted = sorted(nums)
+
+        # Find location of num in nums_sorted, index = number of numbers below it
+        final_list = (nums_sorted.index(num) for num in nums)
+        
+        return final_list
+    
+    # # O(n^2) time | O(1) space
+    # def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+    #     array = []
+    #     for i in range(len(nums)):
+    #         count = 0
+    #         for j in range(len(nums)):
+    #             if i != j and nums[i] > nums[j]:
+    #                 count += 1
+    #         array.append(count)
+    #     return array
         
