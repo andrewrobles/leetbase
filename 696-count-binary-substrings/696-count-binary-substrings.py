4f0744cc5dfc4c93237629db4a1b92e88e41fe6c
Andrew Robles <70864612+andrewrobles@users.noreply.github.com>
2022-06-16 07:45:53 -0700

    Time: 234 ms (56.69%), Space: 14.5 MB (81.59%) - LeetHub

--- 696-count-binary-substrings/696-count-binary-substrings.py
+++ 696-count-binary-substrings/696-count-binary-substrings.py
@@ -1,29 +1,41 @@
 class Solution:
     def countBinarySubstrings(self, s: str) -> int:
-        i = 0
-        count = 0
-        while i < len(s)-1:
-            curr = s[i:i+2]
-            if curr == '01':
-                j = i
-                k = i + 1
-                count += 1
-                while j-1>=0 and k+1<len(s) and s[j-1]=='0' and s[k+1]=='1':
-                    j -= 1
-                    k += 1
-                    curr = s[j:k+1]
-                    count += 1
-            elif curr == '10':
-                j = i
-                k = i + 1
-                count += 1
-                while j-1 >= 0 and k+1 < len(s) and s[j-1]=='1' and s[k+1]=='0':
-                    j -= 1
-                    k += 1
-                    curr = s[j:k+1]
-                    count += 1
-            i += 1
-        return count
+        groups = [1]
+        for i in range(1, len(s)):
+            if s[i-1] != s[i]:
+                groups.append(1)
+            else:
+                groups[-1] += 1
+        ans = 0
+        for i in range(1, len(groups)):
+            ans += min(groups[i-1], groups[i])
+        return ans
+    
+    # def countBinarySubstrings(self, s: str) -> int:
+    #     i = 0
+    #     count = 0
+    #     while i < len(s)-1:
+    #         curr = s[i:i+2]
+    #         if curr == '01':
+    #             j = i
+    #             k = i + 1
+    #             count += 1
+    #             while j-1>=0 and k+1<len(s) and s[j-1]=='0' and s[k+1]=='1':
+    #                 j -= 1
+    #                 k += 1
+    #                 curr = s[j:k+1]
+    #                 count += 1
+    #         elif curr == '10':
+    #             j = i
+    #             k = i + 1
+    #             count += 1
+    #             while j-1 >= 0 and k+1 < len(s) and s[j-1]=='1' and s[k+1]=='0':
+    #                 j -= 1
+    #                 k += 1
+    #                 curr = s[j:k+1]
+    #                 count += 1
+    #         i += 1
+    #     return count
                 
                 
                 
