b03d4c94d4c3341493de8ce51afbc2641d66ec2f
andrewrobles <andrewroblesdev@gmail.com>
2022-08-14 07:55:44 -0700

    Rename test

--- work/run.py
+++ work/run.py
@@ -3,7 +3,7 @@ import work
 import test
 
 class LocalTests(unittest.TestCase):
-	def test_work(self):
+	def test_solution(self):
 		self.assertEqual(work.solution(*test.args), test.output)
 
 if __name__ == '__main__':
