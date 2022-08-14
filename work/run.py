2e783bd4a473984ed2d191e482cbc31d5088ee47
andrewrobles <andrewroblesdev@gmail.com>
2022-08-14 07:52:09 -0700

    Simplify test case entry

--- work/run.py
+++ work/run.py
@@ -4,11 +4,7 @@ import test
 
 class LocalTests(unittest.TestCase):
 	def test_work(self):
-		for case in test.cases:
-			self.assertEqual(
-				work.solution(*case[:-1]),
-				case[-1:][0]
-			)
+		self.assertEqual(work.solution(*test.args), test.output)
 
 if __name__ == '__main__':
 	unittest.main()
